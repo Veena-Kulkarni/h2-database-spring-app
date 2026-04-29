22153a66ba5962f1ad65bb19dac1bfddea26158b
Veena Shridhar Kulkarni <iamveenashridahar@gmail.com>
2026-04-29 21:51:17 +0200

    intial commit to get connect to h2 database

--- src/main/java/com/example/demo/UserEntity.java
+++ src/main/java/com/example/demo/UserEntity.java
@@ -1,4 +1,16 @@
+
 package com.example.demo;
 
+import jakarta.persistence.*;
+
+@Entity
 public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String username;
+
+    // Standard practice: Add getters and setters
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
 }
